b293cd8851c9616ee341ccb90bfbbb6ae8062e46
Alex Thomas <alex-thomas92@outlook.com>
2021-04-07 18:00:20 +0100

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Layout({ children, title }) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <Head>
+          <title>{title}</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const links = [
+    "Home",
+    "Marketing",
+    "Portfolio",
+    "Growth",
+    "About",
+    "Contact",
+  ].map((link, key) => {
+    const router = useRouter();
+
+    const setActiveClasses = (link) => {
+      const linkpath = `/${link}`;
+
+      if (
+        linkpath == router.pathname ||
+        (linkpath == "/Home" && router.pathname == "/")
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    return (
+      <li key={key} className="mr-3">
+        <Link href={link == "Home" ? "/" : `/${link}`}>
+          <a
+            className={`${
+              setActiveClasses(link) ? "font-bold text-white underline" : ""
+            } inline-block px-4 py-2 text-gray-100 hover:text-white hover:underline`}
+          >
+            {link}
+          </a>
+        </Link>
+      </li>
+    );
+  });
+  return (
+    <>
+      <div className="w-full p-2 flex justify-between">
+        <Link href="/contact">
+          <a className="bg-green-300 hover:bg-green-200 text-xs px-2 py-1 rounded">
+            Contact
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a className="bg-green-300 hover:bg-green-200 text-xs px-2 py-1 rounded">
+            Free Consultation
+          </a>
+        </Link>
+      </div>
+      <nav className={`text-white lg:bg-opacity-90 lg:bg-green-300 py-2`}>
+        <div className="container flex flex-wrap items-center justify-between w-full mx-auto mt-0">
+          <div className="flex justify-start pl-4">
+            <Link href="/">
+              <a className="text-2xl font-bold text-white no-underline toggleColour hover:no-underline lg:text-4xl">
+                PT Nutrition Marketing
+              </a>
+            </Link>
+          </div>
+          <div className="block pr-4 lg:hidden">
+            <button
+              id="nav-toggle"
+              className="flex items-center p-1 text-green-800 transition duration-300 ease-in-out transform hover:text-white focus:outline-none focus:shadow-outline hover:scale-105"
+              onClick={toggleOpen}
+            >
+              <svg
+                className="w-6 h-6 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`${
+              isOpen ? "" : "hidden"
+            } z-20 flex-grow w-full p-4 mt-2 text-black bg-green-200 bg-opacity-75 lg:flex lg:items-center lg:w-auto lg:mt-0 lg:bg-transparent lg:p-0`}
+            id="nav-content"
+          >
+            <ul className="items-center justify-end flex-1 list-reset lg:flex">
+              {links}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
