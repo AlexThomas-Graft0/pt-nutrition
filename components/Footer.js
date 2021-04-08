17412ac027f80b7bc6a116aedd5061ac9b202b7a
Alex Thomas <alex-thomas92@outlook.com>
2021-04-08 17:34:16 +0100

    new repo

--- components/Footer.js
+++ components/Footer.js
@@ -1,189 +1,72 @@
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer class="bg-green-500 sm:mt-10 pt-10">
       <div class="max-w-6xl m-auto text-gray-200 flex flex-wrap justify-left">
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
           <div class="text-xs uppercase text-gray-200 font-medium mb-6">
-            Getting Started
+            Services
           </div>
-
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Installation
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Release Notes
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Upgrade Guide
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Using with Preprocessors
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Optimizing for Production
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Browser Support
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            IntelliSense
-          </a>
+          <Link href="/marketing-solutions">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Marketing Solutions
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Our Work
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Growth Tips
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Pricing
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              About Us
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Contact Us
+            </a>
+          </Link>
+          <Link href="/">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"></a>
+          </Link>
         </div>
 
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12"></div>
+        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12"></div>
         <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
           <div class="text-xs uppercase text-gray-200 font-medium mb-6">
-            Core Concepts
+            Legal
           </div>
-
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Utility-First
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Responsive Design
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Hover, Focus, & Other States
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Dark Mode
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Adding Base Styles
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Extracting Components
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Adding New Utilities
-          </a>
-        </div>
-
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-          <div class="text-xs uppercase text-gray-200 font-medium mb-6">
-            Customization
-          </div>
-
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Configuration
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Theme Configuration
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Breakpoints
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Customizing Colors
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Customizing Spacing
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Configuring Variants
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Plugins
-          </a>
-        </div>
-
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-          <div class="text-xs uppercase text-gray-200 font-medium mb-6">
-            Community
-          </div>
-
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            GitHub
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Discord
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            Twitter
-          </a>
-          <a
-            href="#"
-            class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700"
-          >
-            YouTube
-          </a>
+          <Link href="/privacy-policy">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Privacy Policy
+            </a>
+          </Link>
+          <Link href="/terms">
+            <a class="my-3 block text-gray-300 hover:text-gray-50 text-sm font-medium duration-700">
+              Terms &amp; Conditions
+            </a>
+          </Link>
         </div>
       </div>
 
       <div class="pt-2">
         <div class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-gray-200 text-sm flex-col md:flex-row max-w-6xl">
-          <div class="mt-2">© Copyright 1998-year. All Rights Reserved.</div>
+          <div class="mt-2">
+            &copy; pt-nutrition-marketing.co.uk Copyright{" "}
+            {new Date().getFullYear()}. All Rights Reserved.
+          </div>
 
           <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
             <a href="#" class="w-6 mx-1">
