879c9fed79bf125d49b388fcaa13aed96edce527
Alex Thomas <alex-thomas92@outlook.com>
2021-04-18 13:28:57 +0100

    added banner to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,20 @@
 import Head from "next/head";
 
-export default function Layout({ children, title }) {
+export default function Layout({ children, title, banner }) {
+  //bring banner in?
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="w-full flex flex-col items-center justify-center flex-1">
-        <Head>
-          <title>{title}</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        {children}
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {banner}
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="w-full flex flex-col space-y-3 items-center justify-center flex-1 container">
+          {children}
+        </main>
+      </div>
+    </>
   );
 }
--- pages/SEO.js
+++ pages/SEO.js
@@ -1,9 +1,69 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 export default function SEO() {
   return (
-    <Layout title="SEO">
-      <h1>SEO</h1>
-    </Layout>
+    <>
+      <Layout
+        title="SEO"
+        banner={
+          <div className="h-96 bg-header-image bg-cover bg-center flex flex-col justify-center items-center text-white text-center">
+            <h1 className="text-5xl font-bold w-1/3">
+              SEO for PT & Nutritionists
+            </h1>
+          </div>
+        }
+      >
+        <div class="flex w-full">
+          <Image src="/images/ptseo.png" width="761" height="944" />
+          <div class="flex flex-col items-center">
+            <div className="leading text-xl my-2 w-1/2 text-center">
+              Being in the top 3 positions on Google can often mean the
+              difference between an overbooked PT & Nutrtionist business and one
+              that is struggling to get more clients.
+            </div>
+            <div className="leading text-xl my-2 w-1/2 text-center">
+              Through long-term SEO strategies and proven results, we help PT &
+              Nutrtionists rank higher on Google to get more clients.
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-2xl">
+            SEO is not only limited to the common searches like "PT &
+            Nutrtionist in Cardiff".
+          </h2>
+          Overlooked SEO terms to rank for are:
+          <ul>
+            <li>"Fat Loss"</li>
+            <li>"Gender Specific PT"</li>
+            <li>"Healthy Diet"</li>
+            <li>"Toning Diet"</li>
+            <li>and many others</li>
+          </ul>
+          Effective SEO can result in a significant increase in clients which
+          means higher profits for the PT & Nutrtionist partners.
+        </div>
+        <div className="flex flex-col justify-center items-center text-center">
+          <h2 class="text-2xl">The Importance of SEO</h2>
+          <div class="my-2 antialiased tracking-wide">
+            To understand the potential of SEO, if your PT & Nutrtion business
+            ranks in the top 3 positions for "personal trainer Cardiff" which
+            receives over 200+ searches per month, your business could easily
+            receive a minimum of 2-3 clients every month.
+          </div>
+          <div class="my-2 antialiased tracking-wide">
+            Even if we work on a small amount of £100 for a 1:1, 1 hour session
+            x 5 clients = £500 per week, £2,000 per month or £24,000 extra per
+            annum.
+          </div>
+          <div class="my-2 antialiased tracking-wide">
+            It's important to note that this is a minor example. Effective SEO
+            bundled with various marketing strategies can yield significantly
+            higher returns.
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
