cad44c83ddba17a03972520d53d60b2c77e44255
ElJonez <adamhjonesbackup@gmail.com>
2021-04-21 18:13:19 +0100

    Updated more banner images

--- pages/SEO.js
+++ pages/SEO.js
@@ -22,8 +22,8 @@ export default function SEO() {
         banner={
           <div className="h-96 bg-seo-header-image bg-cover bg-center flex flex-col justify-center items-center text-gray-800 text-center">
             <h1 className="text-5xl font-bold w-1/3">SEO</h1>
-            <div className="mt-3 text-2xl w-1/2">
-              Search Engine Optimisation for PT &amp; Nutritionists
+            <div className="text-2xl font-semibold">
+              For PTs &amp; Nutritionalists.
             </div>
           </div>
         }
