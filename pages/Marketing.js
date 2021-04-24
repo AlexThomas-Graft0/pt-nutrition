d9a32e4b81d636000a189dd80b2de9935d2e4728
Alex Thomas <alex-thomas92@outlook.com>
2021-04-24 08:57:05 +0100

    marketing apge

--- pages/Marketing.js
+++ pages/Marketing.js
@@ -20,14 +20,14 @@ export default function Marketing() {
       <Layout
         title="Marketing"
         banner={
-          <div className="h-96 bg-marketing-header-image bg-cover bg-center flex flex-col justify-center items-center text-black text-center">
-            <h1 className="text-5xl font-bold w-1/3">
+          <div className="h-96 bg-marketing-header-image bg-cover bg-center flex flex-col justify-center items-center text-gray-800 text-center">
+            <h1 className="text-4xl md:text-5xl font-bold md:w-1/3">
               Online Marketing Solutions for PTs & Nutritionists
             </h1>
           </div>
         }
       >
-        <div>
+        <div className="px-3">
           <div className="text-xl mt-3 text-center">
             Finding the right marketing company among the multitude of choices
             available is not easy. For this reason we specialize in PTs and
@@ -36,25 +36,33 @@ export default function Marketing() {
           </div>
         </div>
         <Cards cardData={features} />
-        <div className="text-xl text-center justify-center font-bold">
-          Scenario #1: You Already Have A Website
+        {/* scenarios */}
+        <div className="p-3 my-3">
+          <div className="my-3">
+            <h2 className="text-xl text-center font-bold">
+              Scenario #1: You Already Have A Website
+            </h2>
+            <div className="md:text-xl">
+              If you already have a website, was is just created by an
+              individual/company who creates websites that they thought may look
+              good or does it actually work? Does it send the right message
+              across to a potential clients about what they are looking for?
+              Does it fulfill your business objective of getting more patients?
+            </div>
+          </div>
+          <div className="my-3">
+            <h2 className="text-xl text-center font-bold">
+              Scenario #2: You Don't Have A Website Yet
+            </h2>
+            <div className="md:text-xl">
+              If you don't currently have a website, it's extremely important to
+              have one in this competitive market. All credible Pts &amp;
+              Nutrtionists have effective websites that showcase their expertise
+              and experience and it results in more clients and higher profits.
+            </div>
+          </div>
         </div>
-        <div className="text-xl ">
-          If you already have a website, was is just created by an
-          individual/company who creates websites that they thought may look
-          good or does it actually work? Does it send the right message across
-          to a potential clients about what they are looking for? Does it
-          fulfill your business objective of getting more patients?
-        </div>
-        <div className="text-xl text-center justify-center font-bold m-10">
-          Scenario #2: You Don't Have A Website Yet
-        </div>
-        <div className="text-xl text-center justify-center mb-5">
-          If you don't currently have a website, it's extremely important to
-          have one in this competitive market. All credible Pts &amp;
-          Nutrtionists have effective websites that showcase their expertise and
-          experience and it results in more clients and higher profits.
-        </div>
+        {/* scenarios */}
         <FreeAnalysisBanner />
         <BannerBottom />
       </Layout>
